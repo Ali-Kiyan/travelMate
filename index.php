b987ca7c61b3e96b34575b96a075ceb3dedbb378
Ali <alikiyand@gmail.com>
2017-11-16 20:25:13 +0000

    fixing full screen background issue

--- index.php
+++ index.php
@@ -25,16 +25,16 @@
       <div class="login">
         <div class="container">
             <div class="row align-middle">
-                <div class="col-xs-8 col-xs-offset-2">
+                <div class="col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
                     <div class="panel panel-default" style="background-color: rgba(220, 220, 233, 0.3);">
                         <div class="panel-heading">Login</div>
                         <div class="panel-body">
                             <form class="form-horizontal" role="form" method="POST" action="">
 
                                 <div class="form-group">
-                                    <label for="email" class="col-xs-4 control-label">E-Mail Address</label>
+                                    <label for="email" class="col-sm-4 col-xs-4 control-label">E-Mail Address</label>
 
-                                    <div class="col-xs-6">
+                                    <div class="col-sm-4 col-xs-8">
                                         <input id="email" type="email" class="form-control" name="email" value="">
                                             <span class="help-block">
                                                 <strong></strong>
@@ -44,9 +44,9 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <label for="password" class="col-xs-4 control-label">Password</label>
+                                    <label for="password" class="col-sm-4 col-xs-4 control-label">Password</label>
 
-                                    <div class="col-xs-6">
+                                    <div class=" col-sm-4 col-xs-8">
                                         <input id="password" type="password" class="form-control" name="password">
                                             <span class="help-block">
                                                 <strong></strong>
