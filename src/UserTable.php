f7395f573735daf58b2449d671488617a654d753
Ali <alikiyand@gmail.com>
2017-12-31 00:51:57 +0000

    finishing users table

--- src/UserTable.php
+++ src/UserTable.php
@@ -6,4 +6,12 @@ require_once __DIR__ . 'TableAbstract.php';
 class UserTable extends TableAbstract {
   protected $name = 'myusername_address';
   protected $primaryKey = 'id';
+  public function fetchALlUsers() {
+    $results = $this->fetchAll();
+    $userArray = array();
+    while($row = $results->fetch()) {
+      $userArray[] = new User($row);
+    }
+    return $userArray;
+  }
 }
