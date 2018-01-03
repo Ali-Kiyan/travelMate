f0d35602bb3e75d979d7839f106cd69baf4dcfcb
Ali <alikiyand@gmail.com>
2018-01-03 21:33:03 +0000

    adding delete to all locations

--- src/travelMateProject/Location.php
+++ src/travelMateProject/Location.php
@@ -4,11 +4,13 @@ class Location {
     protected $Location_id, $Name, $Description;
     public function __construct($dbrow) {
         $this->Location_id = $dbrow['Location_id'];
+        $this->User_id = $dbrow['User_id'];
         $this->Name = $dbrow['Name'];
         $this->Description = $dbrow['Description'];
     }
     //accessors
     public function getLocationId() { return $this->Location_id;}
+    public function getUserId() { return $this->User_id;}
     public function getName() { return $this->Name;}
     public function getDescription() { return $this->Description;}
 }
