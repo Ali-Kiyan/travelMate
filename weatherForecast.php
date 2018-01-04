ef6d5ef09c23fb7c585cb64f515e01be4fe222ad
Ali <alikiyand@gmail.com>
2018-01-04 20:36:18 +0000

    adding weather forecasting files

---
+++ weatherForecast.php
@@ -0,0 +1,32 @@
+<?php
+
+if(isset($_GET))
+{
+
+$country = $_GET['Country'];
+$city = $_GET['City'];
+//using open weather API
+$api_url
+
+}
+
+
+
+
+
+
+
+
+require_once __DIR__ . '/Views/weatherForecast.phtml';
+
+
+
+
+
+
+
+
+
+
+
+
