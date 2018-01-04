1721f1022cc8e8b85403e181fe3bc28f06952663
Ali <alikiyand@gmail.com>
2018-01-04 21:24:57 +0000

    adding UI layout

--- weatherForecast.php
+++ weatherForecast.php
@@ -6,7 +6,10 @@ if(isset($_GET))
 $country = $_GET['Country'];
 $city = $_GET['City'];
 //using open weather API
-$api_url
+$api_url = "https://api.openweathermap.org/data/2.5/weather?q=" . $city . "," . $country . "&appid=e4b204037b59965c815e80d927e51338";
+$weather_data = file_get_contents($api_url);
+$json = json_decode($weather_data, TRUE);
+   var_dump($json);
 
 }
 
