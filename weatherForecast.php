a097692ded87d027cc6106a206486d396a7e0a34
Ali <alikiyand@gmail.com>
2018-01-04 22:24:09 +0000

    weather api is done

--- weatherForecast.php
+++ weatherForecast.php
@@ -3,21 +3,19 @@
 if(isset($_GET))
 {
 
-$country = $_GET['Country'];
-$city = $_GET['City'];
+$Country = $_GET['Country'];
+$City = $_GET['City'];
 //using open weather API
-$api_url = "https://api.openweathermap.org/data/2.5/weather?q=" . $city . "," . $country . "&appid=e4b204037b59965c815e80d927e51338";
+$api_url = "https://api.openweathermap.org/data/2.5/weather?q=" . $City . "," . $Country . "&appid=e4b204037b59965c815e80d927e51338";
 $weather_data = file_get_contents($api_url);
 $json = json_decode($weather_data, TRUE);
-   var_dump($json);
+$weatherArray['humidity'] = $json['main']['humidity'];
+$weatherArray['currentCondition'] = $json['weather'][0]['main'];
+$weatherArray['windSpeed'] = $json['wind']['speed'];
+$weatherArray['windDirection'] = $json['wind']['deg'];
+$weatherArray['temp'] = $json['main']['temp'];
 
 }
-
-
-
-
-
-
 
 
 require_once __DIR__ . '/Views/weatherForecast.phtml';
