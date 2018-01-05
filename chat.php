47bcd1ab5163a8dcdf90649e5820de41bfb77632
Ali <alikiyand@gmail.com>
2018-01-05 23:45:55 +0000

    chat cleaned

--- chat.php
+++ chat.php
@@ -1,15 +1,23 @@
 <?php
-$view = new stdClass();
-$view->pageTitle = 'Chat';
-require_once __DIR__ . '/vendor/autoload.php';
-
-
-
-
-
-
-
-
-
-//require_once __DIR__ . '/Views/chat.phtml';
-
+require_once __dir__ . "/Views/template/header.phtml";
+require_once __dir__ . "/Views/template/included_functions.php";
+confirm_logged_in ();
+if(isset($_SESSION['User_id']))
+{
+    require_once __dir__ . "/Views/template/nav.phtml";
+}
+?>
+<script src="./lib/JS/jquery-3.2.1.min.js"></script>
+<script src="./lib/JS/chat.js"></script>
+<form action="POST" id="userArea" class="col-sm-8 col-sm-offset-2">
+    <div class="form-group">
+    <label>Message</label>
+    <input type="text" class="form-control" name="messages" id="chattxt" required/>
+    <input type="hidden"  name="User_id" value="<?php $_SESSION['User_id'] ?>"/>
+    <input type="submit" class="btn Brown" value="Post Message"/>
+    </div>
+</form>
+<div id="messages" class="col-sm-8 col-sm-offset-2">
+</div>
+<?php require_once __dir__ . "/Views/template/footer.phtml"
+?>
--- src/travelMateProject/ajaxPostModel.php
+++ src/travelMateProject/ajaxPostModel.php
@@ -2,11 +2,12 @@
 require_once '../../vendor/autoload.php';
 $database = new travelMateProject\ChatTable();
 $message =  $_POST['messages'];
+$user_id = $_POST['User_id'];
 $sql = "INSERT INTO Chat (User_id, Body) VALUES (:User_id, :Body)";
 $results = $database->getdbh()->prepare($sql);
 
 $response  = $results->execute(array(
-    ':User_id' => 81,
+    ':User_id' => $user_id,
     ':Body' => $message
 ));
 
