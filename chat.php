e5c2a4e5339ce802aa9708ce6595dbaf3b186664
Ali <alikiyand@gmail.com>
2018-01-17 21:54:23 +0000

    debuging the chat system

--- chat.php
+++ chat.php
@@ -12,7 +12,7 @@ if(isset($_SESSION['User_id']))
 <form action="POST" id="userArea" class="col-sm-8 col-sm-offset-2">
     <div class="form-group">
     <label>Message</label>
-    <input type="text" class="form-control" name="messages" id="chattxt" required/>
+    <input type="text" class="form-control" name="messages" id="chattxt" required placeholder="write you brief message (Less than 1000 character)">
     <input type="hidden"  name="User_id" value="<?php echo $_SESSION['User_id']; ?>"/>
     <input type="hidden"  name="Username" value="<?php echo $_SESSION['Username']; ?>"/>
     <input type="submit" class="btn Brown" value="Post Message"/>
--- src/travelMateProject/ajaxLoadModel.php
+++ src/travelMateProject/ajaxLoadModel.php
@@ -14,9 +14,8 @@ require_once '../../vendor/autoload.php';
     foreach ($chatArray as $key => $value)
     {
         echo'<div class="chatStyle">';
-        echo '<div class="Brown" style="padding: 10px; font-size: 1.2rem;border-radius: 50px !important; margin-bottom: 14px;">'. '<span style="font-size:0.9rem;">' . $value['Username'] . '</span>' . ' : ' . $value['Body'];
-        echo '<div class="right" style="font-size: 0.9rem;margin-top: 15px;">' . substr((string)$value['Created_at'],11,20) . '</br>' . '</div>';
-        echo'</div>';
+        echo '<div class="Brown" style="padding: 10px; font-size: 1.2rem;border-radius: 50px !important; margin-bottom: 14px;overflow-wrap: break-word;margin-right: 16px !important;">'. '<span style="font-size:0.9rem;">' . $value['Username'] . '</span>' . ' : ' . $value['Body'].' <br><div class="right" style="font-size: 0.9rem;">' . substr((string)$value['Created_at'],11,20) . '</br>' . '</div>';
+        echo '</div>';
 
     }
 
