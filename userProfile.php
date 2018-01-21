f8f6d2cfa148b64780360c41aaeeb9e609c50f5d
Ali <alikiyand@gmail.com>
2018-01-21 03:22:00 +0000

    test

--- userProfile.php
+++ userProfile.php
@@ -1,35 +1,35 @@
 <?php
 //require_once  "./Views/template/included_functions.php";
-confirm_logged_in ();
-$view = new stdClass();
-$view->pageTitle = 'Userprofile';
-require_once  './vendor/autoload.php';
-
-$userdb = new travelMateProject\UserTable();
-$Current_User = $userdb->fetchRecord($_SESSION['User_id']);
-
-if(isset($_POST['Usubmit']))
-{
-    $userdb = new travelMateProject\UserTable();
-    $_SESSION["First_Name"] = $_POST["First_Name"];
-    $_SESSION["Username"] = $_POST["Username"];
-    $_SESSION["Password"] = $_POST["Password"];
-
-
-    $respond = $userdb->editUser($_POST);
-
-    if($respond)
-    {
-        redirect_to("./UserProfile.php");
-        $view->result = '<div class="alert alert-success">Successfully Updated </div>';
-    }
-    else
-    {
-        $view->result = '<div class="alert alert-danger">Please check your input </div>';
-    }
-
-}
-
-
+//confirm_logged_in ();
+//$view = new stdClass();
+//$view->pageTitle = 'Userprofile';
+//require_once  './vendor/autoload.php';
+//
+//$userdb = new travelMateProject\UserTable();
+//$Current_User = $userdb->fetchRecord($_SESSION['User_id']);
+//
+//if(isset($_POST['Usubmit']))
+//{
+//    $userdb = new travelMateProject\UserTable();
+//    $_SESSION["First_Name"] = $_POST["First_Name"];
+//    $_SESSION["Username"] = $_POST["Username"];
+//    $_SESSION["Password"] = $_POST["Password"];
+//
+//
+//    $respond = $userdb->editUser($_POST);
+//
+//    if($respond)
+//    {
+//        redirect_to("./UserProfile.php");
+//        $view->result = '<div class="alert alert-success">Successfully Updated </div>';
+//    }
+//    else
+//    {
+//        $view->result = '<div class="alert alert-danger">Please check your input </div>';
+//    }
+//
+//}
+//
+//
 //require_once './Views/userprofile.phtml';
-
+//
