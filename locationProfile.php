6aa3774d63d02928322aa9b75b42700303f1c178
Ali <alikiyand@gmail.com>
2018-01-22 23:05:12 +0000

    test

--- locationProfile.php
+++ locationProfile.php
@@ -1,40 +1,40 @@
 <?php
-if(!$_GET['More']) {
-    require_once "./Views/template/included_functions.php";
-    confirm_logged_in();
-}
-$view = new stdClass();
-$view->pageTitle = 'Location profile';
-require_once './vendor/autoload.php';
-
-$locationdb = new travelMateProject\LocationTable();
-$Current_Location = $locationdb->fetchRecord($_GET['Location_id']);
-
-
-if(isset($_POST['Usubmit']))
-{
-    $locationdb = new travelMateProject\LocationTable();
-    $respond = $locationdb->editLocation($_POST);
-
-    if($respond)
-    {
-        redirect_to("./locations.php");
-    }
-    else
-    {
-        $view->result = '<div class="alert alert-danger">Please check your input </div>';
-    }
-
-}
-
-
-    if($_GET['More'])
-    {
-        require_once  './Views/location.phtml';
-    }
-    else
-    {
-        require_once  './Views/locationprofile.phtml';
-    }
-
-
+//if(!$_GET['More']) {
+//    require_once "./Views/template/included_functions.php";
+//    confirm_logged_in();
+//}
+//$view = new stdClass();
+//$view->pageTitle = 'Location profile';
+//require_once './vendor/autoload.php';
+//
+//$locationdb = new travelMateProject\LocationTable();
+//$Current_Location = $locationdb->fetchRecord($_GET['Location_id']);
+//
+//
+//if(isset($_POST['Usubmit']))
+//{
+//    $locationdb = new travelMateProject\LocationTable();
+//    $respond = $locationdb->editLocation($_POST);
+//
+//    if($respond)
+//    {
+//        redirect_to("./locations.php");
+//    }
+//    else
+//    {
+//        $view->result = '<div class="alert alert-danger">Please check your input </div>';
+//    }
+//
+//}
+//
+//
+//    if($_GET['More'])
+//    {
+//        require_once  './Views/location.phtml';
+//    }
+//    else
+//    {
+//        require_once  './Views/locationprofile.phtml';
+//    }
+//
+//
